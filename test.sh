3014647376f605e4d0fed916ce36d95145797556
bigbes <bigbes@gmail.com>
2016-03-02 11:31:37 +0300

    Add Travis-CI

---
+++ test.sh
@@ -0,0 +1,5 @@
+curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
+sudo apt-get install -y tarantool tarantool-dev libsmall-dev libmsgpuck-dev cmake --force-yes
+TARANTOOL_DIR=/usr/include cmake . -DCMAKE_BUILD_TYPE=Release
+make
+ctest
